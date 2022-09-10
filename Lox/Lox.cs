f90d28ff6d0231107cf3a349019fe87a8d5c7a54
v.bessonov <bessonov@action-media.ru>
2022-09-11 01:32:24 +0300

    Init

---
+++ Lox/Lox.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Lox
+{
+    /// <summary>
+    /// Lox compiler
+    /// </summary>
+    public class Lox
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length > 1)
+            {
+                Console.WriteLine("Usage: lox [script]");
+                Environment.Exit(64);
+            }
+            else if (args.Length == 1)
+            {
+                RunFile(args[0]);
+            }
+            else
+            {
+                RunPrompt();
+            }
+        }
+
+        private static void RunPrompt()
+        {
+            for (;;)
+            {
+                Console.WriteLine("> ");
+                var line = Console.ReadLine();
+                if (line is null)
+                {
+                    break;
+                }
+
+                Run(line);
+            }
+        }
+
+
+        private static void RunFile(string path)
+        {
+            var content = File.ReadAllText(path);
+            Run(content);
+        }
+
+        private static void Run(string source)
+        {
+            Scanner scanner = new Scanner(source);
+            List<Token> tokens = scanner.scanTokens();
+
+            // For now, just print the tokens.
+            foreach (var token in tokens)
+            {
+                Console.WriteLine(token);
+            }
+        }
+    }
+}
